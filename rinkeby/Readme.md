c90cf8bedaeb4fa620851a3cb974ceef710e378a
Yang Xiao <mike123666@gmail.com>
2022-04-03 18:30:33 -0400

    updated G-Chain contract

---
+++ rinkeby/Readme.md
@@ -0,0 +1,2 @@
+G-Chain Regulatory Contract Address (Rinkeby): 0xCec6fd8Cde51B7BBd92Bdf82314508e1F53081D2
+Regulator Address (Rinkeby): 0x0A4A2f95E8625EB07A67F8DFA0cd566C515A01C3
--- solidity-contracts/G_Chain_Regulate.sol
+++ solidity-contracts/G_Chain_Regulate.sol
@@ -4,20 +4,23 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /** 
  * @title BDSAS_Regulate
- * @dev Implements the G-Chain Regulatory Contract for one L-Chain in BD-SAS. Experimental version 0.2.0
+ * @dev Implements the G-Chain Regulatory Contract for a number (=20) of L-Chains in BD-SAS. Experimental version 0.2.0
  */
 contract BDSAS_Regulate {
 
     struct Server { // SAS server
         string desc; // Description, e.g., name, IP address, other credentials.
-        uint8 status; // 0: valid, 1: invalid
+        uint8 status; // 0: invalid, 1: valid candidate, 2: currently in service
+        uint lid; // L-Chain index that the server currently serves (i.e., status==2) or lastly served
+        uint sequence; // Server sequence for the lid-th L-Chain if status == 2
     }
 
     struct Witness { // SAS server
         string desc; // Description, e.g., name, IP address, other credentials.
-        uint8 status; // 0: valid, 1: invalid
+        uint8 status; // 0: invalid, 1: valid
+        uint lid; // L-Chain index
+        uint sequence; // Witness sequence for the lid-th L-Chain if status == 1
         bool reshuffle_vote; // For confirming a reshuffle proposal
-        bool update_vote; // For confirming an update
     }
 
     struct Regulation { // Short-term regulation, e.g., "order to vacate" due to incumbent arrival
@@ -40,23 +43,25 @@ contract BDSAS_Regulate {
     address public regulator; // There could be multiple regulators, we assume one for convenience
 
     /* L-Chain variables */
-    string public DESC; // Description on the spectrum region, e.g., state-county name
-    string public INTF; // Interference model parameters for the spectrum region
+    string public DESC; // Description on the associated spectrum regions, e.g., state-county names
+    string public INTF; // Interference model parameters for the associated spectrum regions
     uint public T_SHIFT; // The L-Chain's shift length in G-Chain block cycles
-    uint public T_EPOCH; // The L-Chain's epoch length in G-Chain block cycles
-    mapping(address => Server) public Servers;   // Candidate servers
-    mapping(address => Witness) public Witnesses;       // For access control on witnesses
-    address[5] public Witness_Addr;              // We fix 5 witnesses, Witnesses[0] is the anchor
-    address[5] public CurrentServers;            // We require 5 servers to serve an L-Chain
-    uint public shift; // Shift count
-    uint public epoch; // Epoch count
+    uint public T_EPOCH; // The L-Chain's epoch length in G-Chain block cycles 
+    mapping(address => Server) public Servers; // Candidate servers
+    mapping(address => Witness) public Witnesses; // For access control on witnesses
+    address[20][5] public Witnesses_Addr; // We fix 20 L-Chains, 5 witnesses to an L-Chain, with Witnesses[0] being the anchor
+    address[20][5] public CurrServers_Addr; // We require 5 servers to serve an L-Chain
+    uint public shift; // Shift count (shared by all 20 L-Chains)
+    uint[20] public epoch; // Epoch count (for each L-Chain)
     Regulation[] public Regulations; // List of short-term regulations
+    bytes32[20][5] public LocalStates; // Service states of each L-Chain, provided by all serving servers
 
     /* Variables for SAS server reshuffling */
     mapping(address => Proposal) public ReshuffleProposals; // Candidate server' VRF info
-    uint8 public reshuffle_status; // 0: accepting confirmation, 1: confirmation success, 2: confirmation failure (more than half are no-votes)
+    uint8[20] public reshuffle_status; // 0: accepting confirmation, 1: confirmation success, 2: confirmation failure (more than half are no-votes)
     uint public reshuffle_vote_count;
-    address[5] public ProposedServers; // For the next shift
+    uint public reshuffle_yesvote_count;
+    address[20][5] public ProposedServers_Addr; // For the next shift
 
     /** 
      * @dev Create a new regulatory contract.
@@ -64,34 +69,43 @@ contract BDSAS_Regulate {
      */
     constructor(uint t_sh, uint t_ep){
         regulator = msg.sender; // Regulator creates the contract
-        DESC = "This is a test G-Chain contract";
-        INTF = "Test parameters";
+        DESC = "This is a test G-Chain contract for 20 regions.";
+        INTF = "TO FILL: interference model parameters.";
         T_SHIFT = t_sh;
         T_EPOCH = t_ep;
-        reshuffle_status = 1;
-
-        // We fix the 5 witnesses for convenience of testing. In real deployment they could be managed by the regulator
-        Witness_Addr[0] = 0x8057442400c8634B95BA68b76922b2A486FBe4CF; // Also the anchor
-        Witness_Addr[1] = 0xFE7079bbebb5fC8ad4a62fdDeB3556Cb691b2d3E;
-        Witness_Addr[2] = 0xE362321EeddfC2513a54f8427C5ba40d088e8294;
-        Witness_Addr[3] = 0x9463D8301D38341d5f6f7F5304Dd8E3e29867141;
-        Witness_Addr[4] = 0x373E7DbF92e86Bc510F587bA560707A83d4f795E;
-        for(uint i = 0; i < 5; i++) {
-            Witnesses[Witness_Addr[i]].desc = "Fixed witness";
-            Witnesses[Witness_Addr[i]].status = 1; // Valid
-            Witnesses[Witness_Addr[i]].reshuffle_vote = false;
-        }
     }
  
     /**
      * @dev Add an eligible server. Callable by regulator.
-     * @param p: participant address, a: action (0: invalidate, 1: normal), de: description
+     * @param addr: participant address, de: description, li: L-Chain index (0,1,2,...), st: status (0: invalidate, 1: valid, 2: serving as default), seq: valid witness sequence number (0,1,..)
      */
-    function ServerRegister(address p, uint8 a, string memory de) public {
-        require(msg.sender == regulator, "Only regulator can manage participants.");
-        require(a == 0 || a == 1, "Illegal action on server.");
-        Servers[p].status = a;
-        Servers[p].desc = de;
+    function RegisterServer(address addr, string memory de, uint li, uint8 st, uint8 seq) public {
+        require(msg.sender == regulator, "Only regulator can manage participating servers.");
+        require(st == 0 || st == 1 || st == 2, "Illegal status assignment.");
+        Servers[addr].status = st;
+        Servers[addr].desc = de;
+        Servers[addr].lid = li;
+        if(st == 2){ // Default server
+            Servers[addr].sequence = seq;
+            CurrServers_Addr[li][seq] = addr;
+        }
+    }
+
+      /**
+     * @dev Add a witness. Callable by regulator.
+     * @param addr: participant address, de: description, li: L-Chain index (0,1,2,...), sta: status (0: invalidate, 1: valid), seq: valid witness sequence number (0,1,..)
+     */
+    function RegisterWitness(address addr, string memory de, uint li, uint8 st, uint8 seq) public {
+        require(msg.sender == regulator, "Only regulator can manage participating witnesses.");
+        require(st == 0 || st == 1, "Illegal status assignment.");
+        Witnesses[addr].status = st;
+        Witnesses[addr].desc = de;
+        Witnesses[addr].lid = li;
+        Witnesses[addr].reshuffle_vote = false;
+        if(st == 1){
+            Witnesses[addr].sequence = seq;
+            Witnesses_Addr[li][seq] = addr;
+        }
     }
 
     /** 
@@ -108,9 +122,9 @@ contract BDSAS_Regulate {
      * @param pubkey: VRF public key, h1: hash part1, h2: hash part2, p1: proof part1, p2: proof part2, p3: proof part3, 
      */
     function ReshufflePropose(bytes32 pubkey, bytes32 h1, bytes32 h2, bytes32 p1, bytes32 p2, bytes32 p3) public {
-        // By original design, we would need to have all VRF proposals submitted within one epoch near the shift's end
-        // Such time constraints will be added in future; they are absent in this version for easier testing
-        require(Servers[msg.sender].status == 0, "The SAS server does not exist or is no longer valid."); // Valid server
+        // By our original design, we would need to have all VRF proposals submitted within the second last epoch of the current shift. 
+        // However, such time constraints are not implemented in this version for easier testing
+        require(Servers[msg.sender].status == 1 || Servers[msg.sender].status == 2, "The SAS server does not exist or is no longer valid."); // Valid server
         ReshuffleProposals[msg.sender].pubkey = pubkey;
         ReshuffleProposals[msg.sender].hash_1 = h1;
         ReshuffleProposals[msg.sender].hash_2 = h2;
@@ -121,53 +135,70 @@ contract BDSAS_Regulate {
     }
 
     /** 
-     * @dev Collect SAS server group nomination. Callable by the anchor witness.
-     * @param sg: server group nomination by an anchor
+     * @dev Collect SAS server group nomination. Callable by the anchor witness. The VRF verification and SG selection are done off-chain.
+     * @param l: L-Chain index, sg: server group nomination by an anchor
      */
-    function ReshuffleNominate(address[] memory sg) public {
-        require(msg.sender == Witness_Addr[0], "The sender is not the anchor witness."); // Valid anchor witness
-        require(reshuffle_status > 0, "Already nominated; waiting for confirmations.");
+    function ReshuffleNominate(uint l, address[] memory sg) public {
+        require(msg.sender == Witnesses_Addr[l][0], "The sender is not the anchor witness."); // Valid anchor witness
         for(uint i = 0; i < 5; i++) {
-            ProposedServers[i] = sg[i];
+            ProposedServers_Addr[l][i] = sg[i];
         }
         Witnesses[msg.sender].reshuffle_vote = true; // The anchor's own vote
         for(uint i = 1; i < 5; i++) {
-            Witnesses[Witness_Addr[i]].reshuffle_vote = false; // Restart the votes
+            Witnesses[Witnesses_Addr[l][i]].reshuffle_vote = false; // Restart the votes
         }
         reshuffle_vote_count = 1;
-        reshuffle_status = 0;
+        reshuffle_yesvote_count = 1;
+        reshuffle_status[l] = 0;
     }
 
     /** 
-     * @dev Collect SAS server group confirmations. Callable by a normal witness.
+     * @dev Collect SAS server group confirmations. Callable by a normal witness. The VRF verification and nominee verification are done off-chain
+     * @param l: L-Chain index, y: yes vote if true
      */
-    function ReshuffleConfirm() public {
-        require(reshuffle_status == 0, "The confirmation procedure ended.");
-        require(msg.sender != Witness_Addr[0], "Only callable by a normal witness.");
+    function ReshuffleConfirm(uint l, bool y) public returns(uint8){
+        require(reshuffle_status[l] == 0, "The confirmation procedure ended.");
+        require(msg.sender != Witnesses_Addr[l][0], "Only callable by a normal witness.");
         require(Witnesses[msg.sender].status == 1, "Invalid witness.");
         require(Witnesses[msg.sender].reshuffle_vote == false, "You have already voted for this shift.");
-        
-        reshuffle_vote_count ++;
-        Witnesses[msg.sender].reshuffle_vote = true;
-        if(reshuffle_vote_count >= 3) { // 3 out of 5 witnesses vote yes
-            for(uint i = 1; i < 5; i++) {
-                CurrentServers[i] = ProposedServers[i];
-            }
-            reshuffle_status = 1; // Success
+        reshuffle_vote_count++;
+        if(y){
+            reshuffle_yesvote_count++;
         }
+        Witnesses[msg.sender].reshuffle_vote = true;
+        if(reshuffle_yesvote_count >= 3) { // 3 out of 5 witnesses vote yes
+            for(uint i = 0; i < 5; i++) {
+                Servers[CurrServers_Addr[l][i]].status = 1; // Switch the incumbent servers' status from "current" to "valid"
+                CurrServers_Addr[l][i] = ProposedServers_Addr[l][i];
+                Servers[CurrServers_Addr[l][i]].status = 2;
+                Servers[CurrServers_Addr[l][i]].sequence = i;
+            }
+            reshuffle_status[l] = 1; // Success
+        }
+        else if(reshuffle_vote_count == 5) {
+            reshuffle_status[l] = 2; // Reshuffle failure; do nothing; it is up to the regulator to add/delete candidate servers
+        }
+        return(reshuffle_status[l]);
     }
 
     /** 
-     * @dev Allow a server to update its local service. Callable by a server.
-     * @param state: local service state
+     * @dev Allow a server to update its local service. Callable by a current server. Later audits (if needed) will check if all servers for an L-Chain provide the same updates
+     * @param l: L-Chain index, st: local service state (can be digest of L-Chain contract state)
      */
-    function LocalUpdate(bytes32 state) public {
-        // Here we implement a simple compensation scheme:         
+    function LocalUpdate(uint l, bytes32 st) public {
+        // By our original design, we would need to have all servers submit the update within one epoch time from the current epoch ends. 
+        // However, such time constraints are not implemented in this version for easier testing
+        require(Servers[msg.sender].status == 2, "Not a current server."); 
+        require(Servers[msg.sender].lid == l, "Not currently serving the said L-Chain."); 
+        LocalStates[l][Servers[msg.sender].sequence] = st;
+        // TO DO in Future: Compensation scheme (needed in commercial deployment), which may need interaction from witnesses    
     }
 
-    function LocalUpdateConfirm(bytes32 state) public {
-        // Here we implement a simple compensation scheme: 
-        
+    /**
+     * @dev Destroy the contract. Callable by the contract creator, i.e., the regulator
+     */
+    function DestroyContract() public {
+        require(msg.sender == regulator, "Only regulator can destroy the contract.");
+        selfdestruct(payable(regulator));
     }
-
 }
